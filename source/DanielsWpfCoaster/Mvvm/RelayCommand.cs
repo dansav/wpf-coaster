ec3fecea2e38e6a922af8f25172171615af5d44f
Daniel S <dansav@users.noreply.github.com>
2020-01-03 11:03:43 +0100

    allow external control of CanExecute

--- source/DanielsWpfCoaster/Mvvm/RelayCommand.cs
+++ source/DanielsWpfCoaster/Mvvm/RelayCommand.cs
@@ -7,16 +7,30 @@ namespace DanielsWpfCoaster.Mvvm
     {
         readonly Action<object> _execute;
         readonly Predicate<object> _canExecute;
+        private bool _enabled = true;
 
         public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
+
+        }
+
+        public bool Enabled
+        {
+            get => _enabled;
+            set
+            {
+                if (_enabled == value) return;
+
+                _enabled = value;
+                CommandManager.InvalidateRequerySuggested();
+            }
         }
 
         public bool CanExecute(object parameter)
         {
-            return _canExecute?.Invoke(parameter) ?? true;
+            return Enabled && (_canExecute?.Invoke(parameter) ?? true);
         }
 
         public void Execute(object parameter)
